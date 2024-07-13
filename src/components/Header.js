4cfc5930adb56e42570152a630761025a3ba4b05
Vishwa-g-4567 <vishwa.g4567@gmail.com>
2024-07-14 01:37:21 +0530

    Feat: update style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = ({ search, handleSearch }) => {
   return (
     <header className="bg-blue-600 text-white py-4 shadow-lg">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Pokemon Gallery</h1>
+      <div className="container sm:mx-auto px-5 flex justify-between items-center">
+        <h1 className="text-lg sm:text-3xl font-bold">Pokemon Gallery</h1>
         <input
           type="text"
           placeholder="Search Pokemon..."
